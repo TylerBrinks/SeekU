dd55302f4ab70ce3c5ea402aff4a1493749b28e7
tylerbrinks <tylerbrinks@hotmail.com>
2014-01-22 09:43:32 -0700

    Added mongo event and snapshot tests

---
+++ SeekU.Tests/ProviderTests/SeekU.MongoTests/MongoEventStoreFixture.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
+using Moq;
+using NUnit.Framework;
+using SampleDomain.Events;
+using SeekU.Eventing;
+using SeekU.MongoDB;
+using SeekU.MongoDB.Eventing;
+
+namespace SeekU.Tests.ProviderTests.SeekU.MongoTests
+{
+    [TestFixture]
+    public class MongoEventStoreFixture
+    {
+        [Test]
+        public void MongoEventStore_Deserializes_Events()
+        {
+            var id = Guid.NewGuid();
+            var events = new List<EventStream>
+            {
+                new EventStream
+                {
+                    AggregateRootId = id,
+                    DateCreated = DateTime.Now,
+                    EventData = new List<DomainEvent>{ new AccountDebitedEvent(Guid.NewGuid(), 100){ Sequence = 2} },
+                    Id = ObjectId.GenerateNewId(),
+                    SequenceStart = 2,
+                    SequenceEnd = 2
+                },
+                new EventStream
+                {
+                    AggregateRootId = id,
+                    DateCreated = DateTime.Now,
+                    EventData = new List<DomainEvent>{ new AccountCreatedEvent(Guid.NewGuid(), 100){ Sequence = 1} },
+                    Id = ObjectId.GenerateNewId(),
+                    SequenceStart = 1,
+                    SequenceEnd = 1
+                }
+            };
+
+            var database = new Mock<IMongoDataStore>();
+            database.Setup(db => db.GetEventStream(It.IsAny<Guid>(), It.IsAny<long>())).Returns(events);
+
+            var store = new MongoEventStore
+            {
+                GetDatabase = () => database.Object
+            };
+
+            var eventStream = store.GetEvents(id, 1).ToList();
+
+            Assert.AreEqual(2, eventStream.Count());
+            Assert.AreEqual(1, eventStream[0].Sequence);
+            Assert.AreEqual(2, eventStream[1].Sequence);
+            Assert.IsInstanceOf<AccountCreatedEvent>(eventStream[0]);
+            Assert.IsInstanceOf<AccountDebitedEvent>(eventStream[1]);
+        }
+
+        [Test]
+        public void MongoEventStore_Serialized_Events()
+        {
+            var database = new Mock<IMongoDataStore>();
+            database.Setup(db => db.InsertEvents(It.IsAny<EventStream>()));
+
+            var store = new MongoEventStore
+            {
+                GetDatabase = () => database.Object
+            };
+
+            var events = new List<DomainEvent>
+            {
+                new AccountCreatedEvent(Guid.NewGuid(), 100),
+                new AccountDebitedEvent(Guid.NewGuid(), 50)
+            };
+
+            store.Insert(Guid.NewGuid(), events);
+
+            database.Verify(db => db.InsertEvents(It.IsAny<EventStream>()), Times.Once);
+        }
+    }
+}
---
+++ SeekU.Tests/ProviderTests/SeekU.MongoTests/MongoSnapshotStoreFixture.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using SampleDomain.Domain;
+using SeekU.Domain;
+using SeekU.MongoDB;
+using SeekU.MongoDB.Eventing;
+using SeekU.Tests.Properties;
+
+namespace SeekU.Tests.ProviderTests.SeekU.MongoTests
+{
+    [TestFixture]
+    public class MongoSnapshotStoreFixture
+    {
+        [Test]
+        public void MongoSnapshotStore_Returns_Null_When_No_Snapshot_Exists()
+        {
+            var database = new Mock<IMongoDataStore>();
+            database.Setup(db => db.GetSnapshot(It.IsAny<Guid>()));
+
+            var store = new MongoSnapshotStore
+            {
+                GetDatabase = () => database.Object
+            };
+
+            var snapshot = store.GetSnapshot<BankAccountSnapshot>(Guid.NewGuid());
+
+            Assert.IsNull(snapshot);
+        }
+
+        [Test]
+        public void MongoSnapshotStore_Deserializes_Snapshot_Details()
+        {
+            var database = new Mock<IMongoDataStore>();
+            database.Setup(db => db.GetSnapshot(It.IsAny<Guid>())).Returns(new SnapshotDetail{SnapshotData = new BankAccountSnapshot{Balance = 900}});
+
+            var store = new MongoSnapshotStore
+            {
+                GetDatabase = () => database.Object
+            };
+
+            var snapshot = store.GetSnapshot<BankAccountSnapshot>(Guid.NewGuid());
+
+            Assert.AreEqual(900, snapshot.Data.Balance);
+        }
+
+        [Test]
+        public void MongoSnapshotStore_Serializes_Snapshots()
+        {
+            var database = new Mock<IMongoDataStore>();
+            database.Setup(db => db.InsertSnapshot(It.IsAny<SnapshotDetail>()));
+
+            var store = new MongoSnapshotStore
+            {
+                GetDatabase = () => database.Object
+            };
+
+            var snapshot = new Snapshot<BankAccountSnapshot> {Data = new BankAccountSnapshot {Balance = 900}};
+            store.SaveSnapshot(snapshot);
+
+            database.Verify(db => db.InsertSnapshot(It.IsAny<SnapshotDetail>()), Times.Once());
+        }
+    }
+}
